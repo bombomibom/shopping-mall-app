bab5794e017fd50c5570f1fe8482ae41cfac85a3
bomi kim <qhal10048@naver.com>
2022-10-25 18:15:56 +0900

    221025 modify js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,12 @@
 // 로그인
 function loginAction(){
 	
-	console.log($("#userId").val());
-	console.log($("#userPw").val());
+	console.log($("#user-id").val());
+	console.log($("#user-pwd").val());
 	
-	if($("#userId").val() == ""){
+	if($("#user-id").val() == ""){
 		alert("아이디를 입력해주세요.");
-	} else if ($("#userPw").val() == ""){
+	} else if ($("#user-pwd").val() == ""){
 		alert("비밀번호를 입력해주세요.");
 	} else {
 		$.ajax({
